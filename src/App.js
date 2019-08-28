2fc192e79bc01e481c9529c166f3c772e8cda160
codefuldom <dom@codefuldom.com>
2019-08-27 20:53:54 -0500

    :fireworks: changed text in the p tag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          please work!
+          I was changed on the feature branch!
         </p>
         <a
           className="App-link"
