26abbd7366c1cc36a9f9725c39efebe88d1ef97b
codefuldom <dom@codefuldom.com>
2019-08-30 19:50:33 -0500

    :fireworks: inital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>please work!</p>
+        <p>please work pleaaase!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
