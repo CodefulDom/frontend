dbfdc35926b78a1a81e0647ceb389f4a0d61a27d
codefuldom <dom@codefuldom.com>
2019-08-30 19:47:40 -0500

    :fireworks: changed structure on p tag placement

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-import './App.css';
-import logo from './logo.svg';
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I was changed on the feature branch!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>please work!</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
